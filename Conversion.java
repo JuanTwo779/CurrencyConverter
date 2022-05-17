44b64a4a1b64afb5dbbaffbf986020f751710cc0
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-05-17 17:25:38 +1000

    variables, parameters and method fixing
    Added local variables in the main to receive the currency choice to be converted to. Fixed methods to accept the local variables as parameters

--- Conversion.java
+++ Conversion.java
@@ -7,27 +7,39 @@ public class Conversion
     {
     }
 
-    public void currencyHashMap()
-    {
-        //HashMap object called currencyCodes
-        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
+//    public void currencyHashMap()
+//    {
+//        //HashMap object called currencyCodes
+//        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
+//
+//        //keys and values (choice, Country currency)
+//        currencyCodes.put(1, "AUD"); //AUS
+//        currencyCodes.put(2, "MXN"); //Mexico
+//        currencyCodes.put(3, "PHP"); //Philippines
+//        currencyCodes.put(4, "USD"); //United States
+//        currencyCodes.put(5, "GBP"); //Great Britain
+//    }
 
-        //keys and values (choice, Country currency)
-        currencyCodes.put(1, "AUD"); //AUS
-        currencyCodes.put(2, "MXN"); //Mexico
-        currencyCodes.put(3, "PHP"); //Philippines
-        currencyCodes.put(4, "USD"); //United States
-        currencyCodes.put(5, "GBP"); //Great Britain
+    public void centralConvertor(int from, int to, double amount)
+    {
+        if(from == 1)
+        {
+            convertAUS(to, amount);
+        }
     }
 
     /**
      * make single method for all conversions
      * use if else statements?
      */
-    public double convertAUS(double from)
+    public double convertAUS(int to, double amount)
     {
-        double amount = 0;
-        return amount;
+        double convAmount = 0;
+        if (to == 2)
+        {
+            convAmount = amount * 14.04;
+        }
+        return convAmount;
     }
 
 
--- MainClass.java
+++ MainClass.java
@@ -11,13 +11,15 @@ public class MainClass
         Messages objMessage = new Messages();
 
         //variables
-        int choice = 0;
+        int from = 0;
+        int to = 0;
         double amount = 0;
 
         //methods
-        objMessage.Menu();
-        choice = input.acceptIntInput("Which currency do you want to convert from?");
-        amount = input.acceptDoubleInput("How much money do you want to convert?");
+        objMessage.Menu(); //1. Show menu
+        from = input.acceptIntInput("Which currency do you want to convert from?"); //2. ask for original currency
+        to = input.acceptIntInput("Which currency do you want to conver to?"); //3. ask for new currency
+        amount = input.acceptDoubleInput("How much money do you want to convert?"); //4. ask for amount
 
 
 
